c484d1a3f3e8605836645a4df1785914c91a5a9b
Anny27-pixel <jabintasmin102523@gmail.com>
2022-11-09 13:37:36 +0600

    check connection

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div>
-
+      <button className="btn btn-active btn-accent">Button</button>
+      <button className="btn btn-active btn-ghost">Button</button>
     </div>
   );
 }
