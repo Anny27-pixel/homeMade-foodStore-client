f7ae3e74a86cf8f387fd9c45d8bd7cd69ba2d4c6
Anny27-pixel <jabintasmin102523@gmail.com>
2022-11-09 23:35:05 +0600

    reviewr name image review message get

--- src/pages/ServiceDetails/Review.js
+++ src/pages/ServiceDetails/Review.js
@@ -1,20 +1,43 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../context/AuthProvider';
 
-const Review = () => {
+const Review = ({ _id, name }) => {
+
     const { user } = useContext(AuthContext);
+
+
+    const handleReview = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = `${form.name.value}`;
+        const email = user?.email || 'unregistered';
+        const image = `${form.image.value}`;
+        const rating = form.rating.value;
+        const message = form.message.value;
+
+        const review = {
+            Service: _id,
+            ServiceName: name,
+            Reviewer: name,
+            email,
+            image,
+            rating,
+            message
+        }
+    }
+
     return (
         <div>
-            <form>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mx-auto max-w-6xl mb-6'>
-                    <input type="text" placeholder="reviewer name" className="input input-bordered w-full" />
-                    <input type="text" placeholder="Reviewer image" className="input input-bordered w-full " />
-                    <input type="text" placeholder="service ratings" className="input input-bordered w-full " />
-                    <input type="text" placeholder="Reviewer email" defaultValue={user?.email} className="input input-bordered w-full " />
+            <h1 className='text-4xl text-orange-500 font-bold ml-12 mt-6'>Please Review Our Services</h1>
+            <form onSubmit={handleReview}>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mx-auto ml-12 my-6'>
+                    <input name='name' type="text" placeholder="reviewer name" className="input input-bordered w-1/2" />
+                    <input name='image' type="text" placeholder="Reviewer image" className="input input-bordered w-1/2 " />
+                    <input name='rating' type="text" placeholder="service ratings" className="input input-bordered w-1/2 " />
+                    <input name='email' type="text" placeholder="Reviewer email" defaultValue={user?.email} className="input input-bordered w-1/2 " />
                 </div>
-                <div className='max-w-screen-md  mx-auto'>
-                    <textarea style={{ height: '200px', width: '800px' }} className="textarea textarea-bordered " placeholder="Review"></textarea>
-                </div>
+                <textarea name='message' className="textarea textarea-bordered w-1/2 ml-12 " placeholder="Review"></textarea>
+                <input className='btn bg-orange-500 ml-12 my-3 ' type="submit" value="Review" />
             </form>
         </div>
     );
--- src/pages/ServiceDetails/ServiceDetails.js
+++ src/pages/ServiceDetails/ServiceDetails.js
@@ -3,15 +3,25 @@ import { useLoaderData } from 'react-router-dom';
 import Review from './Review';
 
 const ServiceDetails = () => {
-    const { name } = useLoaderData();
+    const { _id, img, name, description, price, rating } = useLoaderData();
     return (
         <div>
-            <div>
-                <h2>Service Section</h2>
-                <h2>{name}</h2>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl ml-12">
+                <figure><img style={{ height: '300px', width: '300px' }} src={img} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{name}</h2>
+                    <p className='text-lg'>{description}</p>
+                    <div className='flex mb-6 text-xl text-orange-500'>
+                        <p>Price: ${price}</p>
+                        <p>Ratings:{rating}</p>
+                    </div>
+                </div>
             </div>
             <div>
-                <Review></Review>
+                <Review
+                    _id={_id}
+                    name={name}
+                ></Review>
             </div>
         </div>
     );
