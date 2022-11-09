2536c2ded0e87d249887f3d4fe6f2ae8f95df3a9
Anny27-pixel <jabintasmin102523@gmail.com>
2022-11-09 14:45:24 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import router from './Router/Routes/Routes';
 
 function App() {
   return (
-    <div>
+    <div data-theme="garden">
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
